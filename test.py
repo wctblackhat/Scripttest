cc59036f98c15d83177f870025383cca4f797893
wctblackhat <61391499+wctblackhat@users.noreply.github.com>
2020-10-12 00:59:33 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,129 +1,24 @@
-print("GAME QUIZ ISLAMI\n")
+class Question:
+     def __init__(self, prompt, answer):
+          self.prompt = prompt
+          self.answer = answer
 
+question_prompts = [
+     "What color are apples?\n(a) Red/Green\n(b)Orange",
+     "What color are bananas?\n(a) Red/Green\n(b)Yellow",
+]
 
-question1 = "SIAPAKAH PENDIRI MASJID AL-AQSHA?"
+questions = [
+     Question(question_prompts[0], "a"),
+     Question(question_prompts[1], "b"),
+]
 
-options1 = "a. Nabi Isa AS\nb. Nabi Nuh AS\nc. Nabi Muhammad SAW\nd. Nabi Ya'kub AS\n"
+def run_quiz(questions):
+     score = 0
+     for question in questions:
+          answer = input(question.prompt)
+          if answer == question.answer:
+               score += 1
+     print("you got", score, "out of", len(questions))
 
-print(question1)
-
-print(options1)
-
-
-
-while True:
-
-    response = input("Silahkan pilih jawaban anda a,b,c atau d\n")
-
-
-
-    if response == "d":
-
-        break
-
-    else:
-        print("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-
-        while True:
-            response = input("Silahkan pilih jawaban anda a,b, atau d\n")
-            if response == "d":
-                stop = True
-                break
-            else:
-                print("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-                stop = True
-                break
-        if stop:
-            break
-while True:
-    bonus = input("JAWABAN BENAR\nApakah kamu ingin mencoba soal bonus?\nTekan 'y' jika ya dan 'n' jika tidak.\n")
-
-    if bonus == "y":
-        print("\n\nSIAPAKAH SAHABAT RASULULLAH SAW YANG MENDAPAT GELAR PEDANG ALLAH TERHUNUS?")
-        print("a. Khalid bin Walid\nb. Abdullah bin Umi Maktum\nc. Mushab bin Umeir\nd. Sa'ad bin Abi Waqqas")
-
-        while True:
-            response = input("\nSilahkan pilih jawaban anda a,b, atau d\n")
-
-            if response == "a":
-                break
-            else:
-                print("Jawaban anda salah, coba lagi !!!")
-
-            while True:
-                response = input("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-
-                if response == "a":
-                    stop = True
-                    break
-                else:
-                    print("jawaban anda belum benar !!, anda telah melampaui batas percobaan")
-                    stop = True
-                    break
-            if stop:
-                break
-        break
-    elif bonus == "n":
-        break
-    else:
-        print("AFWAN !!, SILAHKAN TEKAN 'y' ATAU 'N' UNTUK JAWABAN ANDA")
-
-question1 = "\nBERAPAKAH JUMLAH TENTARA ISLAM DALAM PERANG UHUD?"
-options1 = "a. 300 Orang\nb. 700 Orang\nc. 800 Orang\nd. 900 Orang\n"
-print(question1)
-print(options1)
-
-while True:
-    response = input("Silahkan pilih jawaban anda a,b,c atau d\n")
-
-    if response == "b":
-        break
-    else:
-        print("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-
-        while True:
-            response = input("Silahkan pilih jawaban anda a,b, atau d\n")
-
-            if response == "b":
-                stop = True
-                break
-            else:
-                print("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-                stop = True
-                break
-        if stop:
-            break
-            break
-while True:
-    bonus = input("JAWABAN BENAR\nApakah kamu ingin mencoba soal bonus?\nTekan 'y' jika ya dan 'n' jika tidak.\n")
-
-    if bonus == "y":
-        print("\nBERAPA LAMA MASA UTSMAN BIN AFFAN MENJABAT SEBAGAI KHALIFAH?")
-        print("a. 12 TH\nb. 13 TH\nc. 14 TH\nd. 15 TH")
-
-        while True:
-            response = input("\nSilahkan pilih jawaban anda a,b, atau d\n")
-
-            if response == "a":
-                break
-            else:
-                print("Jawaban anda salah, coba lagi !!!")
-
-            while True:
-                response = input("\n\nJAWABAN ANDA KURANG TEPAT, coba lagi !!!")
-
-
-                if response == "a":
-                    stop = True
-                    break
-                else:
-                    print("jawaban anda belum benar !!, anda telah melampaui batas percobaan")
-                    stop = True
-                    break
-            if stop:
-                break
-        break
-    elif bonus == "n":
-        break
-    else:
-        print("AFWAN !!, SILAHKAN TEKAN 'y' ATAU 'N' UNTUK JAWABAN ANDA")
+run_quiz(questions)
